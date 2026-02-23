bb196a464ec6025d2524c226358e6033df54e25a
Parth-Kale-2008 <parthkale416@gmail.com>
2026-02-24 00:33:29 +0530

    square of the numbers

---
+++ square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class square{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int num = sc.nextInt();
+        num = num*num;
+        System.out.println("the square of the number "+num +" is");
+        
+    }
+}
