d0fce7d9d69537a4264c23e7348b13e09bb4a8f8
Parth-Kale-2008 <parthkale416@gmail.com>
2026-02-28 16:19:47 +0530

    cube of a number

---
+++ cube.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class cube{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int num = sc.nextInt();
+        num = num*num*num;
+        System.out.println("the cube of the number "+num +" is");
+        
+    }
+}
