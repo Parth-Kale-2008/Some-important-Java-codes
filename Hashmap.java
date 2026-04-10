f26763d093b238c718c63bd31073d9651bae153b
Parth Kale <parthkale416@gmail.com>
2026-04-10 22:15:23 +0530

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Hashmap{
+    public static void main(String[] args){
+        HashMap<String,Integer>prn = new HashMap<>();
+        prn.put("parth",998);
+        prn.put("neeraj",886);
+        prn.put("aniruddha",563);
+        prn.put("tanishq",343);
+        prn.put("rudraksh",163);
+
+        System.out.println(prn);
+    }
+}
