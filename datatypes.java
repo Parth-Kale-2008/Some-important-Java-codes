0b46782f42cc0d446869b2ce1bf5a766f44f9027
Parth Kale <parthkale416@gmail.com>
2026-08-03 23:25:41 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes{
+    public static void main(String[] args){
+        char ch = 'a';
+        System.out.println(ch);
+
+        int b = 7;
+        System.out.println(b);
+
+        String str = "parth";
+        System.out.println(str);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float price = 6.7f;
+        System.out.println(price);
+    }
+}
