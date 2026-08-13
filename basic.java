923e03114f0888a69c6610564946c3823fac2f4b
Parth Kale <parthkale416@gmail.com>
2026-08-13 20:30:00 +0530

    Add basic.java

---
+++ basic.java
@@ -0,0 +1,9 @@
+public class basic {
+    char ch = 'A';
+    int number = ch;
+
+    public static void main(String[] args) {
+        basic obj = new basic();
+        System.out.println(obj.number);
+    }
+}
